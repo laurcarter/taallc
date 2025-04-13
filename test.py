7320b301295b26441a8625a551a946694cb5b6b3
laurcarter <62532395+laurcarter@users.noreply.github.com>
2025-04-13 10:11:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -473,6 +473,7 @@ elif st.session_state.step == 11:
     # If the value in the cell is different, override it and highlight the cell
     if current_value != filing_value:
         focus_ws.cell(row=165, column=2, value=filing_value)
+        red_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
         focus_ws.cell(row=165, column=2).fill = red_fill  # Highlight the changed cell
 
     # Save the updated workbook to st.session_state.excel_bytes
